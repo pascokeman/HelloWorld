b378d40123fe5c68353a688d9b735d15733bc3e7
pascokeman <pascokeman001@hotmail.com>
2023-02-02 19:49:10 -0700

    Our First python commit

---
+++ app.py
@@ -0,0 +1,14 @@
+age = 35                    #integer variable
+price = 19.95               #float/double
+first_name = "Ambrose"          #string variable
+is_online = True                #boolean variable
+print(first_name)
+print(age)
+print(price)
+
+clothes = 20
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True
+
+print(patient_name, patient_age, is_new_patient)
